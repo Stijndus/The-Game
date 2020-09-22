8ce3fa98ee09c42ed698037a1e1445d0bafe3fa9
Stijndus <99053054@mydavinci.nl>
2020-09-22 19:18:17 +0200

    Added almost all death endigs

--- the-game.js
+++ the-game.js
@@ -21,12 +21,45 @@ function startGame() {
                                     } else {
                                         verkeerd();
                                     }
+                                } else if (island == "gooi hem weg") {
+                                    alert("Helaas!\nJe hebt de volleybal weggegooid, maar nu wordt je gek van de stemmen in je hoofd\nWaardoor je gek werd");
+                                } else {
+                                    verkeerd();
                                 }
+                            } else if (island = "ontdek het eiland") {
+                                alert("Helaas!\nTijdens het wandelen ben je op een hele giftige slang gaan staan.\n En laten we het erop houden dat je het niet hebt overleeft");
+                            } else {
+                                verkeerd();
                             }
+                        } else if (island == "maak een peddel") {
+                            alert("Helaas!\nJe had zoveel moeite gestoken erin gestoken om je peddel te maken\nDat je gelijk weg wou, maar onderweg werd je te moe door het peddelen\nJe overleed ter plekke")
+                        } else {
+                            verkeerd();
                         }
+                    } else if (island == "zoek voedsel") {
+                        alert("Helaas!\nTijdens het zoeken van eten viel er een kokosnoot op je hoofd");
+                    } else {
+                        verkeerd();
                     }
+                } else if (island == "probeer te ontsnappen") {
+                    alert("Helaas!\nJe probeerde te gaan zwemmen, maar je wist niet dat er haaien om het eiland heen zwommen");
+                } else {
+                    verkeerd();
                 }
+            } else if (island = "zorg voor onderdak") {
+                alert("Helaas!\nJe hebt niet optijd water gevonden, waardoor je overleed");
+            } else {
+                verkeerd();
             }
+        } else if (island = "zoek water") {
+            island = prompt("Je hebt een water bron gevonden\nMaar je begint ook een beetje honger te krijgen\nZoek eten of maak een hutje");
+            if (island == "maak een hutje") {
+                island = prompt("Je hutje is eindelijk klaar maar na zo'n lange dag werken. Heb je nog een keuze\nEten zoeken of een vuurtje maken")
+            } else if (island == "zoek eten") {
+                alert("Helaas!\nHet ging in de avond stormen, en terwijl je lekker aan het genieten was. Werd je getroffen door bliksem")
+            }
+        } else {
+            verkeerd();
         }
     }
 }
